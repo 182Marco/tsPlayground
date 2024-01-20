e9ba445b080451f01c7ff5b1fe54eec39e6cb27c
Marco Milza <marco@MacBook-Pro-di-Marco.local>
2024-01-20 09:03:04 +0100

    updated labels

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as c from './concepts';
 
-console.log(`marcom ---> c.: `, c.OrganizeInSingleObj(c.users));
-console.log(`marcom ---> c.aggregateCars(c.cars): `, c.aggregateCars(c.cars));
-console.log(`marcom ---> piramids: `, c.getAggregate(c.pyramids));
+console.log(`c.OrganizeInSingleObj: `, c.OrganizeInSingleObj(c.users));
+console.log(`c.aggregateCars(c.cars): `, c.aggregateCars(c.cars));
+console.log(`piramids: `, c.getAggregate(c.pyramids));
