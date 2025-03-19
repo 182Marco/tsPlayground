d23f5140b2b5fd6cf0941f4114df2923e8910657
Milza <marcomilza@gmail.com>
2025-03-19 15:14:45 +0100

    removed unused

---
+++ src/concepts/promisesExercises/types/responseModels.ts
@@ -0,0 +1,42 @@
+interface VideoResult {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+  id: string;
+}
+interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface VideoResponse {
+  id: number;
+  results: VideoResult[];
+}
+
+interface MovieResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export { VideoResponse, MovieResponse };
