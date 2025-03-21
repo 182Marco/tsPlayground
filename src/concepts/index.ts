b6fdf0631ec5617dd66e284bc1df98b3c434348b
Milza <marcomilza@gmail.com>
2025-03-21 09:40:50 +0100

    exports

--- src/concepts/index.ts
+++ src/concepts/index.ts
@@ -3,4 +3,6 @@ export * from './exercises';
 export * from './records';
 export * from './promisesExercises';
 export * from './attachTheContext';
-export * from './parallelApiCalls';
+export * from './ApiCalls';
+export * from './tsOverload';
+export * from './pickExrcise';
