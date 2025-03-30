c4f1fc012eef239b29b9d6c6471302cb82129047
Milza <marcomilza@gmail.com>
2025-03-30 20:24:15 +0200

    created class exercise done

---
+++ src/concepts/createClass/createClass.ts
@@ -0,0 +1,35 @@
+class LRUCache {
+  private capacity: number;
+  private cache: Map<number, number>;
+
+  constructor(capacity: number) {
+    this.capacity = capacity;
+    this.cache = new Map();
+  }
+
+  get(key: number): number {
+    if (!this.cache.has(key)) {
+      return -1;
+    }
+    // Move the element to the last position to indicate it was recently used
+    const value = this.cache.get(key)!;
+    this.cache.delete(key);
+    this.cache.set(key, value);
+    return value;
+  }
+
+  put(key: number, value: number): void {
+    if (this.cache.has(key)) {
+      this.cache.delete(key);
+    } else if (this.cache.size >= this.capacity) {
+      // Ensure there is at least one key before deleting
+      const firstKey = this.cache.keys().next().value;
+      if (firstKey !== undefined) {
+        this.cache.delete(firstKey);
+      }
+    }
+    this.cache.set(key, value);
+  }
+}
+
+export { LRUCache };
