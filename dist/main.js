a9f749887989b8228cce15452e4e001c9f622d84
Milza <marcomilza@gmail.com>
2025-03-30 20:26:16 +0200

    ain file updated

--- dist/main.js
+++ dist/main.js
@@ -36,3 +36,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const C = __importStar(require("./concepts"));
 console.log(C.isDog(C.myDog));
 console.log(C.isDog(C.myCat));
+// Test
+const cache = new C.LRUCache(2);
+cache.put(1, 1);
+cache.put(2, 2);
+console.log(cache.get(1)); // 1
+cache.put(3, 3); // Removes key 2
+console.log(cache.get(2)); // -1
+cache.put(4, 4); // Removes key 1
+console.log(cache.get(1)); // -1
+console.log(cache.get(3)); // 3
+console.log(cache.get(4)); // 4
