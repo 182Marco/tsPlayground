755b096715b482d342577156d14fb7a5ac22d6f9
Milza <marcomilza@gmail.com>
2025-04-02 00:01:47 +0200

    updated main

--- dist/main.js
+++ dist/main.js
@@ -34,16 +34,4 @@ var __importStar = (this && this.__importStar) || (function () {
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
 const C = __importStar(require("./concepts"));
-console.log(C.isDog(C.myDog));
-console.log(C.isDog(C.myCat));
-// Test
-const cache = new C.LRUCache(2);
-cache.put(1, 1);
-cache.put(2, 2);
-console.log(cache.get(1)); // 1
-cache.put(3, 3); // Removes key 2
-console.log(cache.get(2)); // -1
-cache.put(4, 4); // Removes key 1
-console.log(cache.get(1)); // -1
-console.log(cache.get(3)); // 3
-console.log(cache.get(4)); // 4
+console.log(Object.getPrototypeOf(C.person));
